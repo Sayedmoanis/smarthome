0a6f9ff1abcf2f493cb9c311cfe29a51b31afb0c
Sayedmoanis <sayedmoanis@gmail.com>
2024-08-12 21:14:56 +0300

    adding timer driver

---
+++ smart home/TIMER_cfg.h
@@ -0,0 +1,11 @@
+
+#ifndef TIMER_CFG_H
+#define TIMER_CFG_H
+
+
+
+
+
+
+
+#endif
---
+++ smart home/TIMER_private.h
@@ -0,0 +1,11 @@
+
+#ifndef TIMER_PRV_H
+#define TIMER_PRV_H
+
+
+
+
+#define MASK_LEAST_3_BIT      0b11111000
+
+
+#endif
